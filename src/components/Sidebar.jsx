6b325dd91645debe4cf7611287788f525e62356d
sheronjay <192441684+sheronjay@users.noreply.github.com>
2025-08-16 23:41:17 +0530

    completed the sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,15 @@
 import { NavLink } from "react-router-dom";
 import "./SideBar.css";
+import logo from "../assets/logo.png";
 
 
 export default function Sidebar() {
   return (
     <aside className="sidebar">
-      <h2>Medea</h2>
+      <div className="logo-title">
+        <img src={logo} alt="Logo" className="logo" />
+        <h2>999 VAULT</h2>
+      </div>
       <nav style={{ marginTop: '1rem' }}>
         <NavLink to="/" >Your Library</NavLink>
         <NavLink to="/now-playing" >Now Playing</NavLink>
