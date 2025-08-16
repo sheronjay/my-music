879f93d4e6e5310e6fe25f58623b6b6465d563b8
sheronjay <192441684+sheronjay@users.noreply.github.com>
2025-08-17 00:20:31 +0530

    added the cards in the library to a flex box. completed the PlayerBar.jsx file.

--- src/components/PlayerBar.jsx
+++ src/components/PlayerBar.jsx
@@ -1,18 +1,44 @@
+import albumcover from "../assets/left-control-album-cover.jpeg";
+
 export default function PlayerBar() {
   return (
-    <footer style={{
-      position: 'fixed',
-      left: 0,
-      right: 0,
-      bottom: 0,
-      background: '#222',
-      color: 'white',
-      height: '64px',
-      padding: '1rem',
-      borderTop: '1px solid #333',
-      zIndex: 1000
-    }}>
-      Player Bar
+    <footer className="player-bar">
+
+
+      <div className="left">
+        <img src={albumcover} alt="Album Cover" className="cover" style={{ borderRadius: "8px", width: "64px", height: "64px" }} />
+        <div className="track-info">
+          <div className="title">Long Gone</div>
+          <div className="artist">Juice WRLD</div>
+        </div>
+        <button className="like">♡</button>
+      </div>
+
+
+      <div className="center">
+        <div className="controls">
+          <button>🔁</button>
+          <button>⏮️</button>
+          <button className="play">▶️</button>
+          <button>⏭️</button>
+          <button>🔀</button>
+        </div>
+
+        <div className="progress-bar">
+          <span>2:49</span>
+          <div className="bar-bg">
+            <div className="bar-fill"></div>
+          </div>
+          <span>3:37</span>
+        </div>
+      </div>
+
+
+      <div className="right">
+        <button>🖥️</button>
+        <button>🔊</button>
+        <input type="range" className="volume" />
+      </div>
     </footer>
-  );
+  )
 }
--- src/pages/Library/Library.jsx
+++ src/pages/Library/Library.jsx
@@ -4,15 +4,19 @@ export default function Library(){
     return(
         <><div className="Recent">
             <h3>Recently Added</h3>
-            <div className="cards"></div>
-            <div className="cards"></div>
-            <div className="cards"></div>
+            <div className="cards">
+                <div className="card"></div>
+                <div className="card"></div>
+                <div className="card"></div>
+            </div>
         </div>
         <div className="Library">
             <h3>Your Library</h3>
-            <div className="cards"></div>
-            <div className="cards"></div>
-            <div className="cards"></div>
+            <div className="cards">
+                <div className="card"></div>
+                <div className="card"></div>
+                <div className="card"></div>
+            </div>
         </div></>
     );
 }
