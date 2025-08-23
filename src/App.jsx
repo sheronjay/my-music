a598edd3d7202c58832538335e6587025056e57d
sheronjay <192441684+sheronjay@users.noreply.github.com>
2025-08-23 10:48:06 +0530

    test-card is not working

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AppShell from "./layout/AppShell";
+import CardTest from "./pages/CardTest"; // We'll create this
 
 export default function App() {
-    return <AppShell />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AppShell />} />
+        <Route path="/test-card" element={<CardTest />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+import album from "../assets/left-control-album-cover.jpeg";
+import "./Card.css";
+
+export default function Card(){
+    return(
+        <div className="card">
+            <img src={album} alt="Album Cover" className="card-image" />
+            <div className="card-content">
+                <h3 className="card-title">Album Title</h3>
+                <h3 className="card-artist">Artist Name</h3>
+            </div>
+
+        </div>
+    )
+}
